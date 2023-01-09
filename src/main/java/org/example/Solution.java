f74ba7f7c47ebf46a78969765d4ff547dcee2820
annt <annt@gft.com>
2023-01-09 16:35:21 +0000

    DOne

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,29 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+
+        int result=0;
+
+        for (int[] ints : grid) {
+
+            Arrays.sort(ints);
+        }
+
+        for(int i=0;i<grid[0].length;i++){
+
+            int max=Integer.MIN_VALUE;
+
+            for (int[] ints : grid) {
+
+                max = Math.max(ints[i], max);
+            }
+
+            result+=max;
+        }
+
+        return result;
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,42 @@
-package com.example;public class Solution {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    int result;
+
+    int[][] grid;
+
+    static Solution solution;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyOneValor(){
+
+        result = 1;
+
+        grid = new int[][] {{1}};
+
+        assertEquals(result, solution.deleteGreatestValue(grid));
+    }
+
+    @Test
+    public void multipleValors(){
+
+        result = 8;
+
+        grid = new int[][] {{1,2,4}, {3,3,1}};
+
+        assertEquals(result, solution.deleteGreatestValue(grid));
+    }
 }
